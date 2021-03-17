cf2c11a84214c10c8d9205a8c9a644bbee52bc70
Seb_Leib <sebastien.leib1@gamil.com>
2021-03-17 13:27:47 +0800

    Initial OOP: 17/03/21

--- ascii_generator.py
+++ ascii_generator.py
@@ -1,25 +1,12 @@
 import math
 import cv2                                      # python -m pip install opencv-python
 import numpy as np                              # python -m pip install numpy
-import time
-import json
+from time import time
 from tqdm import trange                         # python -m pip install tqdm
 from PIL import Image, ImageDraw, ImageFont
 
 
 ascii_order = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
-
-
-def read_json():
-    with open("font_data.json") as f:
-        data = json.load(f)
-
-    return data["fonts"]
-
-
-def write_json(data):
-    with open("font_data.json", "w") as f:
-        json.dump(data, f, indent=2)
 
 
 def find_char(gray):
@@ -34,133 +21,117 @@ def find_char(gray):
 
 
 class ASCII_Generator:
-    def __init__(self, fontname="JetBrainsMono-Regular", fontsize=15):
-        # 1 check if settings already exists in json
-        data = read_json()
-        font_data = data[fontname]
+    def __init__(self, mode="color"):
+        # This is where we want to start working with fonttools
+        self.font = ImageFont.truetype("fonts\\JetBrainsMono-Regular.ttf", 15)
+        self.mode = mode
+        self.char_width = 10
+        self.char_height = 20
 
-        supported_fontnames = font_data.keys()
-        if fontname not in supported_fontnames:
-            init_fontname(fontname, fontsize)
+    def _img_rgb_to_ascii(self, img_rgb, w, h, scale):
+        img_rgb = img_rgb.resize((w, h), Image.ANTIALIAS)
 
-        else:
-            supported_fonsizes = [i[1] for i in font_data[fontname]]
-            if fontsize not in supported_fonsizes:
-                init_fontsize()
+        img_out = Image.new("RGB", (w * self.char_width,
+                                    h * self.char_height), (0, 0, 0))
+        d = ImageDraw.Draw(img_out)
 
-        try:
-            self.font = ImageFont.truetype(
-                "fonts\\%s.ttf" % fontname, fontsize)
-        except FileNotFoundError:
-            print(
-                "File %s.ttf cannot be found in the font directory." % fontname)
+        pix_data = img_rgb.load()
 
-        # 2 creates new setting
+        for i in range(h):
+            for j in range(w):
+                rgb = pix_data[j, i]
+                gray = sum(rgb) / 3
+                d.text((j * self.char_width, i * self.char_height),
+                       find_char(gray), font=self.font, fill=rgb)
 
+        return img_out
+
+    def _vid_to_ascii(self, frame_array, scale):
+
+        output_frame_array = []
+
+        w, h = frame_array[0].size
+        w, h = int(scale * w), int(scale * h *
+                                   (self.char_width / self.char_height))
+
+        # for f in frame_array:
+        #     f = Image.fromarray(f)
+        #     output_frame_array.append(img_rgb_to_ascii(
+        #         f, fontname, scale, char_width, char_height, fontsize))
+
+        for k in trange(len(frame_array)):
+            img_out = self._img_rgb_to_ascii(frame_array[k], w, h, scale)
+            output_frame_array.append(img_out)
+
+        # w, h = w*self.char_width, h*self.char_height
+        w, h = output_frame_array[0].size
+
+        return output_frame_array, w, h
+        # return output_frame_array
+
+    def convert_img(self, image_path, output_file="output.png", scale=0.08):
+        print("Converting Image...")
+
+        t1 = time()
+
+        img_rgb = Image.open(image_path).convert("RGB")
+
+        w, h = img_rgb.size
+        w, h = (int(scale * w), int(scale * h *
+                                    (self.char_width / self.char_height)))
+
+        img_out = self._img_rgb_to_ascii(img_rgb, w, h, scale)
+        img_out.save(output_file)
+
+        t2 = time()
+        print("\nImage %s has been successfully generated from %s, completed in %g seconds" %
+              (output_file, image_path, round(t2-t1, 2)))
+
+    def convert_vid(self, video_path, output_file="output.mp4", scale=0.05):
+        print("Converting Video...")
+        t1 = time()
+
+        vid = cv2.VideoCapture(video_path)
+
+        frame_array = []
+
+        # Can't use multiprocessing, or at least I don't think you can
+        print("\nSplitting video into its individual frames...")
+        for f in trange(int(vid.get(cv2.CAP_PROP_FRAME_COUNT))):
+            _, img_bgr = vid.read()
+            # try:
+            img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+            img_rgb = Image.fromarray(img_rgb)
+            frame_array.append(img_rgb)
+            # except:
+            #     break
+
+        print("\nConverting each frame to ASCII.")
+        output_frame_array, w, h = self._vid_to_ascii(frame_array, scale)
+        # output_frame_array = self._vid_to_ascii(frame_array, scale)
+        # w, h = output_frame_array[0].size
+
+        fps = vid.get(cv2.CAP_PROP_FPS)
+        vid.release()
+
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        output_vid = cv2.VideoWriter(output_file, fourcc, fps, (w, h))
+
+        print("\nWriting frames to new file.")
+        for i in range(len(output_frame_array)):
+            # try:
+            img_bgr = np.array(output_frame_array[i])
+            output_vid.write(cv2.cvtColor(img_bgr, cv2.COLOR_RGB2BGR))
+            # except:
+            #     output_vid.release()
+            #     break
+
+        cv2.destroyAllWindows()
+        output_vid.release()
+
+        t2 = time()
+        print("Video %s has been successfully generated from %s, completed in %g seconds." %
+              (output_file, video_path, round(t2-t1, 2)))
+
+    def convert_vid_to_dir(self, dir, output_dir="output", scale=0.05):
         pass
-    # take character order from json
-
-
-def img_rgb_to_ascii(img, fontname, scale, char_width, char_height, fontsize):
-    w, h = img.size
-    img = img.resize(
-        (int(scale * w), int(scale * h * (char_width / char_height))), Image.ANTIALIAS)
-    w, h = img.size
-
-    font = ImageFont.truetype("fonts\\%s.ttf" % fontname, fontsize)
-
-    output_img = Image.new("RGB", (w * char_width, h * char_height), (0, 0, 0))
-    d = ImageDraw.Draw(output_img)
-
-    img_rgb = img.load()
-
-    for i in range(h):
-        for j in range(w):
-            rgb = img_rgb[j, i]
-            gray = sum(rgb) / 3
-            d.text((j * char_width, i * char_height),
-                   find_char(gray), font=font, fill=rgb)
-
-    return output_img
-
-
-def convert_img_from_path(image_path, output_file="output", fontname="JetBrainsMono-Regular", scale=0.08, char_width=10, char_height=20, fontsize=15):
-    t1 = time.time()
-
-    img = Image.open(image_path).convert("RGB")
-
-    output_img = img_rgb_to_ascii(img, fontname, scale,
-                                  char_width, char_height, fontsize)
-    output_img.save("%s.png" % output_file)
-
-    t2 = time.time()
-
-    print("Image %s.png has been successfully generated from %s, completed in %f seconds" %
-          (output_file, image_path, round(t2-t1, 2)))
-
-
-def vid_to_ascii(frame_array, fontname, scale, char_width, char_height, fontsize):
-    t1 = time.time()
-
-    output_frame_array = []
-
-    # for f in frame_array:
-    #     f = Image.fromarray(f)
-    #     output_frame_array.append(img_rgb_to_ascii(
-    #         f, fontname, scale, char_width, char_height, fontsize))
-
-    for i in trange(len(frame_array)):
-        f = Image.fromarray(frame_array[i])
-        output_frame_array.append(img_rgb_to_ascii(
-            f, fontname, scale, char_width, char_height, fontsize))
-
-    t2 = time.time()
-
-    print(f"Images to ASCII took {round(t2-t1,2)} seconds.")
-
-    return output_frame_array
-
-
-def convert_vid_from_path(video_path, output_file="output", fontname="JetBrainsMono-Regular", scale=0.08, char_width=10, char_height=20, fontsize=15):
-    t1 = time.time()
-
-    vid = cv2.VideoCapture(video_path)
-
-    frame_array = []
-
-    # Can't use multiprocessing, or at least I don't think you can
-    for f in range(int(vid.get(cv2.CAP_PROP_FRAME_COUNT))):
-        _, img_bgr = vid.read()
-        try:
-            frame_array.append(cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB))
-        except:
-            break
-
-    output_frame_array = vid_to_ascii(
-        frame_array, fontname, scale, char_width, char_height, fontsize)
-
-    w, h = output_frame_array[0].size
-
-    fps = vid.get(cv2.CAP_PROP_FPS)
-    output_vid = cv2.VideoWriter(
-        "%s.mp4" % output_file, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-
-    for i in range(len(output_frame_array)):
-        try:
-            output_vid.write(cv2.cvtColor(
-                np.array(output_frame_array[i]), cv2.COLOR_RGB2BGR))
-        except:
-            output_vid.release()
-            break
-
-    cv2.destroyAllWindows()
-    vid.release()
-    output_vid.release()
-
-    t2 = time.time()
-    print("Video %s.mp4 has been successfully generated from %s, completed in %d seconds." %
-          (output_file, video_path, round(t2-t1, 2)))
-
-
-def convert_vid_from_dir(dir, output_dir="output", fontname="JetBrainsMono-Regular", scale=0.08, char_width=10, char_height=20, fontsize=15):
-    pass
--- run_generator.py
+++ run_generator.py
@@ -1,9 +1,29 @@
-from ascii_generator import convert_img_from_path, convert_vid_from_path
+from ascii_generator import ASCII_Generator
 
-# Make sure the file you intend to convert is in the same directory
+# Make sure the file you intend to convert is in the same directory as run_generator.py & ascii_generator.py
 
-# convert_img_from_path works with .jpg & .png
-# for example: convert_img_from_path("example.jpg")
+""" Step 1: Initializing the generator object
+    for example:
 
-# convert_vid_from_path works with .mp4 & .mov
-# convert_vid_from_path("Glowing_Open_Fire__Videvo.mov")
+    gen = ASCII_Generator()
+"""
+
+"""
+    Step 2: Calling class-methods to run the conversion
+    convert_img works with .jpg & .png
+    the output image can be .png(default)
+    for example: 
+
+    gen.convert_img("example.jpg", output_file="example_out.png", scale=0.08)
+
+    convert_vid works with .mp4 & .mov
+    the output video can be .mp4(default)
+    for example: gen.convert_vid("example.mov")
+
+    gen.convert_vid("example.mov", output_file="example_out.mp4", scale=0.05)
+"""
+
+gen = ASCII_Generator()
+gen.convert_vid("shimmer.mp4", "shimmer_out.mp4")
+# gen.convert_vid("spinning_dodecahedron.mp4", "spinning_dodecahedron_out.mp4")
+# gen.convert_img("Pilot Wallpaper.png", "Pilot Wallpaper_out.png")
